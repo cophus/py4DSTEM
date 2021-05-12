fd3ecc8cc6a579c12e08770c45baf68cf7d33f4a
Colin Ophus <cophus@gmail.com>
2021-05-11 21:55:10 -0700

    More tests

--- py4DSTEM/visualize/vis_interactive.py
+++ py4DSTEM/visualize/vis_interactive.py
@@ -9,9 +9,10 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 # from bqplot import pyplot as plt
 from bqplot import *
 
-
-
-
+from bqplot.interacts import (
+    FastIntervalSelector, IndexSelector, BrushIntervalSelector,
+    BrushSelector, MultiSelector, LassoSelector,
+)
 
 
 def browse4D(data4D, figsize=(16,8)):
@@ -39,17 +40,36 @@ def browse4D(data4D, figsize=(16,8)):
     				np.round(data4D.data.shape[3]/2).astype(int)))
 
     # define coordinate axes
-    x = np.arange(data4D.data.shape[0])
-    y = np.arange(data4D.data.shape[1])
-    qx = np.arange(data4D.data.shape[2])
-    qy = np.arange(data4D.data.shape[3])
+    x = np.arange(data4D.data.shape[1])
+    y = np.arange(data4D.data.shape[0])
+    qx = np.arange(data4D.data.shape[3])
+    qy = np.arange(data4D.data.shape[2])
 
 
     # Get image slices
     real_im = data4D.data[:,:,qxy[0],qxy[1]]
     diff_im = data4D.data[xy[0],xy[1],:,:]
+    real_im = np.flip(real_im, axis=0)
+    diff_im = np.flip(diff_im, axis=0)
+    diff_im = np.maximum(diff_im,0)**0.1
 
+    # create_panel(BrushSelector, orientation='vertical', scale=scales['y'])
+    # create_panel(BrushSelector)
+
+    # Make marks for the 2 panels
     fig = [];
+
+
+
+    # Diffraction
+    # selector = BrushSelector
+    # sel = selector(marks=[scatter], x_scale=scales['x'], y_scale=scales['y'])
+    # text_brush = HTML()
+    # if selector != LassoSelector:
+    #     def update_text(*args):
+    #         text_brush.value = '{}.selected = {}'.format(selector.__name__, sel.selected)
+    #     sel.observe(update_text, 'selected')
+    #     update_text()
 
     diff_x_sc = LinearScale()
     diff_y_sc = LinearScale()
@@ -58,14 +78,30 @@ def browse4D(data4D, figsize=(16,8)):
                         y=qy, 
                         color=diff_im,
                         scales={'x': diff_x_sc, 'y': diff_y_sc, 'color': diff_col_sc})
-    diff_ax_x = Axis(scale=diff_x_sc)
+    diff_ax_x = Axis(scale=diff_x_sc, visible=False)
     diff_ax_y = Axis(scale=diff_y_sc, 
-                     orientation='vertical')
+                     orientation='vertical', visible=False)
     diff_ax_c = ColorAxis(scale=diff_col_sc)
+
+
+    # selector = BrushSelector
+    # sel = selector(marks=[diff_heat], x_scale=diff_x_sc, y_scale=diff_y_sc)
+    # # sel = BrushSelector(marks=[diff_heat], x_scale=scales['x'], y_scale=scales['y'])
+    # text_brush = HTML()
+    # def update_text(*args):
+    #     text_brush.value = '{}.selected = {}'.format(selector.__name__, sel.selected)
+    # sel.observe(update_text, 'selected')
+    # update_text()
+
+
     fig.append(Figure(marks=[diff_heat], axes=[diff_ax_x, diff_ax_y, diff_ax_c],
                      title='Mean Diffraction Pattern',
-                     layout=Layout(width='450px', height='450px'),
+                     layout=Layout(width='500px', height='500px'),
                      min_aspect_ratio=1, max_aspect_ratio=1, padding_y=0))
+
+
+    # display(diff_fig)
+    # display(widgets.HBox(fig))
 
     real_x_sc = LinearScale()
     real_y_sc = LinearScale()
@@ -74,175 +110,139 @@ def browse4D(data4D, figsize=(16,8)):
                         y=y, 
                         color=real_im,
                         scales={'x': real_x_sc, 'y': real_y_sc, 'color': real_col_sc})
-    real_ax_x = Axis(scale=real_x_sc)
+    real_ax_x = Axis(scale=real_x_sc, visible=False)
     real_ax_y = Axis(scale=real_y_sc, 
-                orientation='vertical')
+                orientation='vertical', visible=False)
     real_ax_c = ColorAxis(scale=real_col_sc)
     fig.append(Figure(marks=[real_heat], axes=[real_ax_x, real_ax_y, real_ax_c],
                      title='Virtual Detector Image',
-                     layout=Layout(width='450px', height='450px'),
+                     layout=Layout(width='500px', height='500px'),
                      min_aspect_ratio=1, max_aspect_ratio=1, padding_y=0))
 
-
-    display(widgets.HBox(fig, align_content = 'fill'))
-
-    fig
-
-    # fig = []
-    # fig.append(Figure(layout=fig_layout, 
-    #               axes=[ax_x, ax_y], 
-    #               marks=[line], 
-    #               title='Example 1', 
-    #               fig_margin = dict(top=30, bottom=10, left=20, right=20)
-    #               ))
-
-    # size = 100
-    # scale = 100.
-    # np.random.seed(0)
-    # x_data = np.arange(size)
-    # y_data = np.cumsum(np.random.randn(size)  * scale)
-
-    # x_sc = LinearScale()
-    # y_sc = LinearScale()
-
-    # ax_x = Axis(label='X', scale=x_sc, grid_lines='solid')
-    # ax_y = Axis(label='Y', scale=y_sc, orientation='vertical', grid_lines='solid')
-
-    # line = Lines(x=x_data, y=x_data, scales={'x': x_sc, 'y': y_sc})
-
-    # fig_layout = widgets.Layout(width='auto', height='auto')
+    display(widgets.HBox(fig))
 
 
-    # figx = list()
-    # figx[0] = Figure(layout=fig_layout, 
-    #               axes=[ax_x, ax_y], 
-    #               marks=[line], 
-    #               title='Example 1', 
-    #               fig_margin = dict(top=30, bottom=10, left=20, right=20)
-    #               )
-    # figx[1] = Figure(layout=fig_layout, 
-    #               axes=[ax_x, ax_y], 
-    #               marks=[line], 
-    #               title='Example 2', 
-    #               fig_margin = dict(top=30, bottom=10, left=20, right=20)
-    #               )
-    # display(widgets.HBox(figx, align_content = 'stretch'))
-    # figy=[]
-    # for i in range(2):
-    #     figx=[]
-    #     for j in range(3):
-        #     figx.append(Figure(layout=fig_layout, axes=[ax_x, ax_y], marks=[line], title='Example' + str(i*3+j), fig_margin = dict(top=30, bottom=10, left=20, right=20)))
-        # figy.append(widgets.HBox(figx))
-    # display(widgets.VBox(figy, align_content = 'stretch'))
+    # diff_x_sc = LinearScale()
+    # diff_y_sc = LinearScale()
+    # diff_col_sc = ColorScale(scheme=cmap_diff)
+    # diff_heat = HeatMap(x=qx, 
+    #                     y=qy, 
+    #                     color=diff_im,
+    #                     scales={'x': diff_x_sc, 'y': diff_y_sc, 'color': diff_col_sc})
+    # diff_ax_x = Axis(scale=diff_x_sc, visible=False)
+    # diff_ax_y = Axis(scale=diff_y_sc, 
+    #                  orientation='vertical', visible=False)
+    # diff_ax_c = ColorAxis(scale=diff_col_sc)
+    # fig.append(Figure(marks=[diff_heat], axes=[diff_ax_x, diff_ax_y, diff_ax_c],
+    #                  title='Mean Diffraction Pattern',
+    #                  layout=Layout(width='500px', height='500px'),
+    #                  min_aspect_ratio=1, max_aspect_ratio=1, padding_y=0))
 
-    # Generate plots
-    # diff_axes_options = {'color': {'orientation': "vertical","side":"left"}}
-    # fig = plt.figure(title='4D Browser')
-    # plt.heatmap(diff_im, axes_options=diff_axes_options)
-
-    # bar_chart = plt.bar([1, 2, 3, 4, 5], [2, 9, 10, 40, 40])
-    # line_chart = plt.plot([1, 2, 3, 4, 5], [10, 5, 30, 60, 20], 'rs-')
-    # fig
-
-    # fig  = plt.figure(title="Mean Diffraction Pattern",
-    #                        padding_y=0)
-    # fig.layout.width = "900px"
-    # fig.layout.height = "450px"
-
-    # diff_axes_options = {'color': {'orientation': "vertical","side":"left"},
-    #                     'scales': {}
-    #                     }
-    # diff_plot = plt.heatmap(diff_im, axes_options=diff_axes_options)
-
-    # real_axes_options = {'color': {'orientation': "vertical","side":"right"}}
-    # real_plot = plt.heatmap(real_im, axes_options=real_axes_options)
-
-    # plt.show()
-# fig
-
-    # diff_fig  = plt.figure(title="Mean Diffraction Pattern",
-    #                        padding_y=0)
-    # diff_fig.layout.width = "900px"
-    # diff_fig.layout.height = "450px"
-    # diff_axes_options = {'color': {'orientation': "vertical","side":"left"}}
-    # diff_plot = plt.heatmap(diff_im, axes_options=diff_axes_options)
+    # real_x_sc = LinearScale()
+    # real_y_sc = LinearScale()
+    # real_col_sc = ColorScale(scheme=cmap_real)
+    # real_heat = HeatMap(x=x, 
+    #                     y=y, 
+    #                     color=real_im,
+    #                     scales={'x': real_x_sc, 'y': real_y_sc, 'color': real_col_sc})
+    # real_ax_x = Axis(scale=real_x_sc, visible=False)
+    # real_ax_y = Axis(scale=real_y_sc, 
+    #             orientation='vertical', visible=False)
+    # real_ax_c = ColorAxis(scale=real_col_sc)
+    # fig.append(Figure(marks=[real_heat], axes=[real_ax_x, real_ax_y, real_ax_c],
+    #                  title='Virtual Detector Image',
+    #                  layout=Layout(width='500px', height='500px'),
+    #                  min_aspect_ratio=1, max_aspect_ratio=1, padding_y=0))
 
 
+
+    # create_figure(BrushSelector, x_scale=scales['x'], y_scale=scales['y'])
+
+
+
+# def create_panel(selector, **selector_kwargs):
+#     '''
+#     Returns a Figure with a Scatter and a Selector.
     
-    # plt.show()
+#     Arguments
+#     ---------
+#     selector: The type of Selector, one of
+#         {'BrushIntervalSelector', 'BrushSelector', 'FastIntervalSelector', 'IndexSelector', 'LassoSelector'}
+#     selector_kwargs: Arguments to be passed to the Selector
+#     '''
+#     diff_x_sc = LinearScale()
+#     diff_y_sc = LinearScale()
+#     diff_col_sc = ColorScale(scheme=cmap_diff)
+#     diff_heat = HeatMap(x=qx, 
+#                         y=qy, 
+#                         color=diff_im,
+#                         scales={'x': diff_x_sc, 'y': diff_y_sc, 'color': diff_col_sc})
+#     diff_ax_x = Axis(scale=diff_x_sc, visible=False)
+#     diff_ax_y = Axis(scale=diff_y_sc, 
+#                      orientation='vertical', visible=False)
+#     diff_ax_c = ColorAxis(scale=diff_col_sc)
+#     fig = Figure(marks=[diff_heat], axes=[diff_ax_x, diff_ax_y, diff_ax_c],
+#                      title='Mean Diffraction Pattern',
+#                      layout=Layout(width='500px', height='500px'),
+#                      min_aspect_ratio=1, max_aspect_ratio=1, padding_y=0,
+#                      interaction=sel)
 
 
-    # real_fig  = plt.figure(title="Mean Diffraction Pattern",padding_y=0)
-    # real_fig.layout.width = "200px"
-    # real_fig.layout.height = "200px"
-    # real_axes_options = {'color': {'orientation': "vertical","side":"left"}}
-    # plt.heatmap(real_im, axes_options=real_axes_options)
-    # plt.show()
+#         # scatter = Scatter(x=dates_actual, y=prices, scales=scales, colors=['orange'],
+#         #                   selected_style={'opacity': '1'}, unselected_style={'opacity': '0.2'})
+#     sel = selector(marks=[scatter], **selector_kwargs)
+    
+#     text_brush = HTML()
+#     if selector != LassoSelector:
+#         def update_text(*args):
+#             text_brush.value = '{}.selected = {}'.format(selector.__name__, sel.selected)
+#         sel.observe(update_text, 'selected')
+#         update_text()
+
+#         # x_ax = Axis(label='Index', scale=scales['x'])
+#         # x_ay = Axis(label=(symbol + ' Price'), scale=scales['y'], orientation='vertical')
+#         # fig = Figure(marks=[scatter], axes=[x_ax, x_ay], title='{} Example'.format(selector.__name__),
+#         #              interaction=sel)
 
 
 
+#     return VBox([fig, text_brush])
 
 
-    # # initialize figure and axes
-    # fig,ax = plt.subplots(1,2,figsize=figsize)
+# from pandas import DataFrame
+# class AdderDOEProblem_PlotUtils:
+#     def __init__(self, parseutils):
+#         self.data = DataFrame({'timestamps': parseutils.getTimestampValues(),
+#                             'Adder.sum': parseutils.getValues('Adder.sum'),
+#                             'Adder.a': parseutils.getValues('desvar_a.a'),
+#                             'Adder.b': parseutils.getValues('desvar_b.b')})
+#         # step size
+#         self.Adder_a__step = 0.1
+#         self.Adder_b__step = 0.1
 
-    # # divider = make_axes_locatable(ax)
-    # # cax = divider.append_axes('right', size='5%', pad=0.05)
+#         # axes configuration
+#         x_axis_values = self.data['Adder.a']
+#         y_axis_values = self.data['Adder.sum']
 
-    # # im = ax.imshow(im_real, cmap='bone')
+#         self.fig = plt.figure(title='AdderDOEProblem')
+#         self.p = plt.plot(x_axis_values, y_axis_values)
 
-    # # fig.colorbar(im, cax=cax, orientation='vertical')
-    # # plt.show()
+#         w_a_slider = widgets.FloatSlider(value=0, min=0, max=1, step=self.Adder_a__step, description='Adder.a')
+#         w_b_slider = widgets.FloatSlider(value=0, min=0, max=1, step=self.Adder_b__step, description='Adder.b')
+#         self.widgets_list = [w_a_slider, w_b_slider]
 
-    # # # plot the initial images
-    # h0 = ax[0].imshow(im_diff, cmap=cmap_diff)
-    # h1 = ax[1].imshow(im_real, cmap=cmap_real)
+#     def update (self, change):
+#         # Placeholder logic for testing 
+#         self.p.y = [i+1 for i in self.p.y]
 
-    # # appearance of diffraction image
-    # ax[0].axis('off')
-    # divider0 = make_axes_locatable(ax[0])
-    # cax0 = divider0.append_axes('left', size='5%', pad=0.10)
-    # fig.colorbar(h0, cax=cax0, orientation='vertical')
-    # cax0.yaxis.set_ticks_position('left')
-
-    # # appearance of real space image
-    # ax[1].axis('off')
-    # divider1 = make_axes_locatable(ax[1])
-    # cax1 = divider1.append_axes('right', size='5%', pad=0.10)
-    # fig.colorbar(h1, cax=cax1, orientation='vertical')
-
-
-    # # Interactive elements
-    # def update_qx(qx):
-    #     f.layout.xaxis.range = [start, end]
-
-    # # Add diffraction slider bar selectors
-    # interact(draw_browse_4D, qx=(0,data4D.data.shape[0]));
-
-
-    # # initial axis 0 appearance
-    # ax[0].axis('off')
-    # divider = make_axes_locatable(ax[0])
-    # cax = divider.append_axes('left', size='5%', pad=0.10)
-    # ax[0].colorbar(h0, cax=cax, orientation='vertical')
-
-    # ax[1].axis('off')
-
-
-    # for col in ax:
-    #     im = col.imshow(data, cmap='bone')
-    #     divider = make_axes_locatable(col)
-    #     cax = divider.append_axes('right', size='5%', pad=0.05)
-    #     fig.colorbar(im, cax=cax, orientation='vertical')
-    # plt.show()
-
-    # return True
-    # cax = ax.matshow(_ar,vmin=vmin,vmax=vmax,cmap=cmap,**kwargs)
-    #     if np.any(_ar.mask==True):
-    #         mask_display = np.ma.array(data=_ar.mask,mask=_ar.mask==False)
-    #         cmap = ListedColormap([mask_color,'w'])
-    #         ax.matshow(mask_display,cmap=cmap)
-
-
-# def draw_browse_4D(qx):
-# 	print(qx)
+# #     def plot (self):
+# #         plt.show()
+# #         for w in self.widgets_list:
+# #             w.observe(self.update, 'value')
+# #         self.update(None)
+# #         widgets.HBox([widgets.VBox(self.widgets_list), self.fig])
+#     def plot (self):
+#         for w in self.widgets_list:
+#             w.observe(self.update, 'value')
+#         self.update(None)
+#         return widgets.HBox([widgets.VBox(self.widgets_list), self.fig])
